d4c9557eded8c44621c3e6f1d105606c930f6321
gaps3600 <gustavo.sandoval.privados87@gmail.com>
2026-08-01 13:12:36 -0600

    fix: habilitar ssl para postgresql en render

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Configuración de conexión a PostgreSQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
 });
 
 // Inicialización de la Tabla de Base de Datos
